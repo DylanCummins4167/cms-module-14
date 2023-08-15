2c30ec3cba9999b0fdffc9c11583b75b8c6ec52d
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-08-14 20:57:58 -0500

    Update idleTimeout.js

--- middleware/idleTimeout.js
+++ middleware/idleTimeout.js
@@ -4,3 +4,11 @@ const idleTimeout = (timeoutDuration) => {
   return (req, res, next) => {
     // Get the token from the request header
     const token = req.header('x-auth-token');
+
+     if (token) {
+      // Verify the token
+      jwt.verify(token, 'secretkey', (err, decoded) => {
+        if (err) {
+          // Token is invalid or expired
+          return res.status(403).json({ error: 'Session expired. Please log in again.' });
+        }

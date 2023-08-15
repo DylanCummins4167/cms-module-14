1982a69f4523733f6e7005631550a375b81dbcfb
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-08-14 20:58:14 -0500

    Update idleTimeout.js

--- middleware/idleTimeout.js
+++ middleware/idleTimeout.js
@@ -12,3 +12,7 @@ const idleTimeout = (timeoutDuration) => {
           // Token is invalid or expired
           return res.status(403).json({ error: 'Session expired. Please log in again.' });
         }
+
+        // Calculate the time elapsed since the token was issued
+        const currentTime = Date.now();
+        const tokenIssuedTime = decoded.iat * 1000; // Convert seconds to milliseconds           
